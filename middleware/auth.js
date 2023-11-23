cd86e76a27abded0eea65e4209f6bcaf7a726a4d
Dobberman <pangestubima89@gmail.com>
2023-11-23 11:41:11 +0700

    add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const {User} = require("../models");
+const {verify_token} = require("../lib/crypto");
+
+module.exports = {
+    isAuth: async (req, res, next) => {
+        try {
+            const {token} = req.headers;
+            if (!token) {
+                throw {
+                    code: 401
+                };
+            }
+            const claim = await verify_token(token).catch(()=>{
+                throw {
+                    code: 401
+                };
+            });
+            if (!claim) {
+                throw {
+                    code: 401
+                };
+            }
+            req.user = await User.findByPk(claim.id, {});
+            if (!req.user) {
+                throw {
+                    code: 401
+                };
+            }
+            next();
+        }catch (err) {
+            res.sendStatus(err.code || 500);
+        }
+    }
+}
