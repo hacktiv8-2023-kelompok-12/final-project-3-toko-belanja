b5fb4be85c8e0f8d6bf9b5eefe0e2c3f45aa63dc
Dobberman <pangestubima89@gmail.com>
2023-12-01 12:31:54 +0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const route = require("./routes");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(route);
